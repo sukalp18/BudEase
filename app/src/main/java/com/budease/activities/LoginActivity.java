5da836dad42782cfa3974466336a6a6dcea2923c
Amlan <amlanroutray88@gmail.com>
2018-10-24 11:50:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/budease/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.budease.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.budease.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
