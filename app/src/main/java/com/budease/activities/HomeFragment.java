c8aa40448d3aa7c55c4b2e4672ea0931f660832c
sukalp18 <44337801+sukalp18@users.noreply.github.com>
2018-11-01 10:05:12 +0530

    Add files via upload

---
+++ app/src/main/java/com/budease/activities/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.example.ashwini.budeasetest1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_calls_fragment, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+}
---
+++ app/src/main/java/com/budease/activities/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.ashwini.budeasetest1;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+
+    //viewPager
+    private ViewPager viewPager;
+
+    ViewPagerAdapter adapter;
+
+    //Fragments
+
+    ChatFragment chatFragment;
+    HomeFragment homeFragment;
+    ContactsFragment contactsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setOffscreenPageLimit(3);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setTabTextColors(Color.parseColor("#CEFFFFFF"),Color.parseColor("#ffffff"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition(),false);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.getTabAt(position).select();
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_status:
+                Toast.makeText(this, "Home Status Click", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Home Settings Click", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment=new HomeFragment();
+        chatFragment=new ChatFragment();
+        contactsFragment=new ContactsFragment();
+        adapter.addFragment(homeFragment,"CALLS");
+        adapter.addFragment(chatFragment,"CHAT");
+        adapter.addFragment(contactsFragment,"CONTACTS");
+        viewPager.setAdapter(adapter);
+    }
+
+}
